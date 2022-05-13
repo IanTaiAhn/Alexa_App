e654601a71a71ad531c612a20c425e6ec514c9d3
iantaiahn <iantaiahn@yahoo.com>
2022-05-12 21:39:51 -0600

    First test

--- src/main/java/com/weber/cs3230/ChatBotMain.java
+++ src/main/java/com/weber/cs3230/ChatBotMain.java
@@ -4,5 +4,7 @@ public class ChatBotMain {
 
     public static void main(String[] args) {
         System.out.println("assignment 1 base up and running");
+        System.out.println("first committed change");
+
     }
 }
