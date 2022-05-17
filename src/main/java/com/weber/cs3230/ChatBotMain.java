755809345e8b70d6fde03e6af1d056443912a761
iantaiahn <iantaiahn@yahoo.com>
2022-05-16 18:56:48 -0600

    deleted print statements.

--- src/main/java/com/weber/cs3230/ChatBotMain.java
+++ src/main/java/com/weber/cs3230/ChatBotMain.java
@@ -3,9 +3,6 @@ package com.weber.cs3230;
 public class ChatBotMain {
 
     public static void main(String[] args) {
-        System.out.println("assignment 1 base up and running");
-        System.out.println("first committed change");
-        System.out.println("second change lets give it a go!");
-        System.out.println("doing it again but with the git commands using the terminal");
+
     }
 }
