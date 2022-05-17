9d8ad7e1422ff795088dfae9bf5d754f61abe43f
iantaiahn <iantaiahn@yahoo.com>
2022-05-16 18:56:13 -0600

    Welp i guess i'll just use the ide haha

--- src/main/java/com/weber/cs3230/ChatBotMain.java
+++ src/main/java/com/weber/cs3230/ChatBotMain.java
@@ -6,5 +6,6 @@ public class ChatBotMain {
         System.out.println("assignment 1 base up and running");
         System.out.println("first committed change");
         System.out.println("second change lets give it a go!");
+        System.out.println("doing it again but with the git commands using the terminal");
     }
 }
