fb35f1808db9ca6258e32da9f61f15dd0e76d399
iantaiahn <iantaiahn@yahoo.com>
2022-05-16 18:44:20 -0600

    Second Test pushed and committed, using ide git tools.

--- src/main/java/com/weber/cs3230/ChatBotMain.java
+++ src/main/java/com/weber/cs3230/ChatBotMain.java
@@ -5,6 +5,6 @@ public class ChatBotMain {
     public static void main(String[] args) {
         System.out.println("assignment 1 base up and running");
         System.out.println("first committed change");
-
+        System.out.println("second change lets give it a go!");
     }
 }
