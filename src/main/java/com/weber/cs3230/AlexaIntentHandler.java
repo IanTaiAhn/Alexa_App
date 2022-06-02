fbf529cb9a6937d3b142e43e5736263084a864cb
iantaiahn <iantaiahn@yahoo.com>
2022-06-01 22:04:54 -0600

    Deleted comments

--- src/main/java/com/weber/cs3230/AlexaIntentHandler.java
+++ src/main/java/com/weber/cs3230/AlexaIntentHandler.java
@@ -9,15 +9,7 @@ public class AlexaIntentHandler {
 
     public Answer handleIntent(@PathVariable String intentString) {
         AlexaIntent intent = AlexaIntent.getIntentFromString(intentString);
-        /*
-         * Once you finish what the assignment says to do, you'll be able to do something like this:
-         *Make this compile!
-         *
-         * AnswerGenerator answerGenerator = intent.getAnswerGenerator();
-         * return new Answer(answerGenerator.getAnswerText());
-         */
          AnswerGenerator answerGenerator = intent.getAnswerGenerator();
          return new Answer(answerGenerator.getAnswerText());
-//        return new Answer("nice job dood " + intentString); //TODO delete me! And get what's up there working
     }
 }
